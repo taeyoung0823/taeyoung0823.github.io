608a7d197d30127e22442770ae2eedefe63f4f40
kimxodud <kimxodud0823@naver.com>
2024-05-28 22:52:41 +0900

    script_fix

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 async function setupCamera() {
     const video = document.getElementById('video');
-    const screenWidth = window.innerWidth;
-    const screenHeight = window.innerHeight;
-
     const stream = await navigator.mediaDevices.getUserMedia({
         video: {
             width: { ideal: 720 },
@@ -11,9 +8,10 @@ async function setupCamera() {
         }
     });
     video.srcObject = stream;
-
     return new Promise((resolve) => {
         video.onloadedmetadata = () => {
+            video.width = video.videoWidth;
+            video.height = video.videoHeight;
             resolve(video);
         };
     });
@@ -27,11 +25,9 @@ async function setupPoseNet(video) {
 function calculateAngle(a, b, c) {
     const ab = [b.x - a.x, b.y - a.y];
     const bc = [c.x - b.x, c.y - b.y];
-
     const dotProduct = ab[0] * bc[0] + ab[1] * bc[1];
     const magnitudeAB = Math.sqrt(ab[0] * ab[0] + ab[1] * ab[1]);
     const magnitudeBC = Math.sqrt(bc[0] * bc[0] + bc[1] * bc[1]);
-
     const angle = Math.acos(dotProduct / (magnitudeAB * magnitudeBC));
     return angle * (180 / Math.PI);
 }
@@ -90,6 +86,9 @@ async function detectPose(video, net) {
     const canvas = document.getElementById('output');
     const ctx = canvas.getContext('2d');
     
+    canvas.width = video.width;
+    canvas.height = video.height;
+
     let lastCountTime = Date.now();
     let count = 0;
     const countDisplay = document.getElementById('count');
@@ -104,15 +103,12 @@ async function detectPose(video, net) {
 
     const recentAngles = [];
     const maxRecentAngles = 10;
-
     async function poseDetectionFrame() {
         const pose = await net.estimateSinglePose(video, {
             flipHorizontal: false
         });
-
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
         if (pose) {
             drawKeypoints(pose.keypoints, 0.6, ctx);
             drawSkeleton(pose.keypoints, 0.7, ctx);
@@ -121,10 +117,13 @@ async function detectPose(video, net) {
                 const leftKnee = pose.keypoints.find(point => point.part === 'leftKnee' && point.score > 0.5);
                 const leftHip = pose.keypoints.find(point => point.part === 'leftHip' && point.score > 0.5);
                 const leftAnkle = pose.keypoints.find(point => point.part === 'leftAnkle' && point.score > 0.5);
+                const rightKnee = pose.keypoints.find(point => point.part === 'rightKnee' && point.score > 0.5);
+                const rightHip = pose.keypoints.find(point => point.part === 'rightHip' && point.score > 0.5);
+                const rightAnkle = pose.keypoints.find(point => point.part === 'rightAnkle' && point.score > 0.5);
 
                 if (leftKnee && leftHip && leftAnkle) {
-                    const kneeAngle = calculateAngle(leftHip.position, leftKnee.position, leftAnkle.position);
-                    recentAngles.push(kneeAngle);
+                    const leftKneeAngle = calculateAngle(leftHip.position, leftKnee.position, leftAnkle.position);
+                    recentAngles.push(leftKneeAngle);
 
                     if (recentAngles.length > maxRecentAngles) {
                         recentAngles.shift();
@@ -132,14 +131,36 @@ async function detectPose(video, net) {
 
                     const averageAngle = recentAngles.reduce((sum, angle) => sum + angle, 0) / recentAngles.length;
                     const squatEvaluation = evaluateSquat(averageAngle);
-                    console.log(`Squat Evaluation: ${squatEvaluation}, Knee Angle: ${averageAngle.toFixed(2)}`);
+                    console.log(`Squat Evaluation: ${squatEvaluation}, Left Knee Angle: ${averageAngle.toFixed(2)}`);
 
                     ctx.font = "30px Arial";
                     ctx.fillStyle = "red";
                     ctx.fillText(`Squat: ${squatEvaluation}`, 10, 50);
 
                     const currentTime = Date.now();
-                    if (squatEvaluation === "Perfect" && currentTime - lastCountTime > 1000) {
+                    if (squatEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
+                        count += 1;
+                        lastCountTime = currentTime;
+                    }
+                }
+                if (rightKnee && rightHip && rightAnkle) {
+                    const rightKneeAngle = calculateAngle(rightHip.position, rightKnee.position, rightAnkle.position);
+                    recentAngles.push(rightKneeAngle);
+
+                    if (recentAngles.length > maxRecentAngles) {
+                        recentAngles.shift();
+                    }
+
+                    const averageAngle = recentAngles.reduce((sum, angle) => sum + angle, 0) / recentAngles.length;
+                    const squatEvaluation = evaluateSquat(averageAngle);
+                    console.log(`Squat Evaluation: ${squatEvaluation}, Right Knee Angle: ${averageAngle.toFixed(2)}`);
+
+                    ctx.font = "30px Arial";
+                    ctx.fillStyle = "red";
+                    ctx.fillText(`Squat: ${squatEvaluation}`, 10, 50);
+
+                    const currentTime = Date.now();
+                    if (squatEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
                         count += 1;
                         lastCountTime = currentTime;
                     }
@@ -150,8 +171,9 @@ async function detectPose(video, net) {
                 const leftAnkle = pose.keypoints.find(point => point.part === 'leftAnkle' && point.score > 0.5);
                 const rightKnee = pose.keypoints.find(point => point.part === 'rightKnee' && point.score > 0.5);
                 const rightAnkle = pose.keypoints.find(point => point.part === 'rightAnkle' && point.score > 0.5);
+                const rightHip = pose.keypoints.find(point => point.part === 'rightHip' && point.score > 0.5);
 
-                if (leftKnee && leftHip && leftAnkle && rightKnee && rightAnkle) {
+                if (leftKnee && leftHip && leftAnkle && rightKnee && rightAnkle && rightHip) {
                     const frontKneeAngle = calculateAngle(leftHip.position, leftKnee.position, leftAnkle.position);
                     const backKneeAngle = calculateAngle(rightHip.position, rightKnee.position, rightAnkle.position);
                     const lungeEvaluation = evaluateLunge(frontKneeAngle, backKneeAngle);
@@ -162,7 +184,24 @@ async function detectPose(video, net) {
                     ctx.fillText(`Lunge: ${lungeEvaluation}`, 10, 50);
 
                     const currentTime = Date.now();
-                    if (lungeEvaluation === "Perfect" && currentTime - lastCountTime > 1000) {
+                    if (lungeEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
+                        count += 1;
+                        lastCountTime = currentTime;
+                    }
+                }
+
+                if (rightKnee && rightHip && rightAnkle && leftKnee && leftAnkle && leftHip) {
+                    const frontKneeAngle = calculateAngle(rightHip.position, rightKnee.position, rightAnkle.position);
+                    const backKneeAngle = calculateAngle(leftHip.position, leftKnee.position, leftAnkle.position);
+                    const lungeEvaluation = evaluateLunge(frontKneeAngle, backKneeAngle);
+                    console.log(`Lunge Evaluation: ${lungeEvaluation}, Front Knee Angle: ${frontKneeAngle.toFixed(2)}, Back Knee Angle: ${backKneeAngle.toFixed(2)}`);
+
+                    ctx.font = "30px Arial";
+                    ctx.fillStyle = "red";
+                    ctx.fillText(`Lunge: ${lungeEvaluation}`, 10, 50);
+
+                    const currentTime = Date.now();
+                    if (lungeEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
                         count += 1;
                         lastCountTime = currentTime;
                     }
@@ -171,18 +210,37 @@ async function detectPose(video, net) {
                 const leftElbow = pose.keypoints.find(point => point.part === 'leftElbow' && point.score > 0.5);
                 const leftShoulder = pose.keypoints.find(point => point.part === 'leftShoulder' && point.score > 0.5);
                 const leftWrist = pose.keypoints.find(point => point.part === 'leftWrist' && point.score > 0.5);
+                const rightElbow = pose.keypoints.find(point => point.part === 'rightElbow' && point.score > 0.5);
+                const rightShoulder = pose.keypoints.find(point => point.part === 'rightShoulder' && point.score > 0.5);
+                const rightWrist = pose.keypoints.find(point => point.part === 'rightWrist' && point.score > 0.5);
 
                 if (leftElbow && leftShoulder && leftWrist) {
-                    const elbowAngle = calculateAngle(leftShoulder.position, leftElbow.position, leftWrist.position);
-                    const shoulderPressEvaluation = evaluateShoulderPress(elbowAngle);
-                    console.log(`Shoulder Press Evaluation: ${shoulderPressEvaluation}, Elbow Angle: ${elbowAngle.toFixed(2)}`);
+                    const leftElbowAngle = calculateAngle(leftShoulder.position, leftElbow.position, leftWrist.position);
+                    const shoulderPressEvaluation = evaluateShoulderPress(leftElbowAngle);
+                    console.log(`Shoulder Press Evaluation: ${shoulderPressEvaluation}, Left Elbow Angle: ${leftElbowAngle.toFixed(2)}`);
 
                     ctx.font = "30px Arial";
                     ctx.fillStyle = "red";
                     ctx.fillText(`Shoulder Press: ${shoulderPressEvaluation}`, 10, 50);
 
                     const currentTime = Date.now();
-                    if (shoulderPressEvaluation === "Perfect" && currentTime - lastCountTime > 1000) {
+                    if (shoulderPressEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
+                        count += 1;
+                        lastCountTime = currentTime;
+                    }
+                }
+
+                if (rightElbow && rightShoulder && rightWrist) {
+                    const rightElbowAngle = calculateAngle(rightShoulder.position, rightElbow.position, rightWrist.position);
+                    const shoulderPressEvaluation = evaluateShoulderPress(rightElbowAngle);
+                    console.log(`Shoulder Press Evaluation: ${shoulderPressEvaluation}, Right Elbow Angle: ${rightElbowAngle.toFixed(2)}`);
+
+                    ctx.font = "30px Arial";
+                    ctx.fillStyle = "red";
+                    ctx.fillText(`Shoulder Press: ${shoulderPressEvaluation}`, 10, 50);
+
+                    const currentTime = Date.now();
+                    if (shoulderPressEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
                         count += 1;
                         lastCountTime = currentTime;
                     }
@@ -191,18 +249,37 @@ async function detectPose(video, net) {
                 const leftElbow = pose.keypoints.find(point => point.part === 'leftElbow' && point.score > 0.5);
                 const leftShoulder = pose.keypoints.find(point => point.part === 'leftShoulder' && point.score > 0.5);
                 const leftWrist = pose.keypoints.find(point => point.part === 'leftWrist' && point.score > 0.5);
+                const rightElbow = pose.keypoints.find(point => point.part === 'rightElbow' && point.score > 0.5);
+                const rightShoulder = pose.keypoints.find(point => point.part === 'rightShoulder' && point.score > 0.5);
+                const rightWrist = pose.keypoints.find(point => point.part === 'rightWrist' && point.score > 0.5);
 
                 if (leftElbow && leftShoulder && leftWrist) {
-                    const elbowAngle = calculateAngle(leftShoulder.position, leftElbow.position, leftWrist.position);
-                    const dumbbellCurlEvaluation = evaluateDumbbellCurl(elbowAngle);
-                    console.log(`Dumbbell Curl Evaluation: ${dumbbellCurlEvaluation}, Elbow Angle: ${elbowAngle.toFixed(2)}`);
+                    const leftElbowAngle = calculateAngle(leftShoulder.position, leftElbow.position, leftWrist.position);
+                    const dumbbellCurlEvaluation = evaluateDumbbellCurl(leftElbowAngle);
+                    console.log(`Dumbbell Curl Evaluation: ${dumbbellCurlEvaluation}, Left Elbow Angle: ${leftElbowAngle.toFixed(2)}`);
 
                     ctx.font = "30px Arial";
                     ctx.fillStyle = "red";
                     ctx.fillText(`Dumbbell Curl: ${dumbbellCurlEvaluation}`, 10, 50);
 
                     const currentTime = Date.now();
-                    if (dumbbellCurlEvaluation === "Perfect" && currentTime - lastCountTime > 1000) {
+                    if (dumbbellCurlEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
+                        count += 1;
+                        lastCountTime = currentTime;
+                    }
+                }
+
+                if (rightElbow && rightShoulder && rightWrist) {
+                    const rightElbowAngle = calculateAngle(rightShoulder.position, rightElbow.position, rightWrist.position);
+                    const dumbbellCurlEvaluation = evaluateDumbbellCurl(rightElbowAngle);
+                    console.log(`Dumbbell Curl Evaluation: ${dumbbellCurlEvaluation}, Right Elbow Angle: ${rightElbowAngle.toFixed(2)}`);
+
+                    ctx.font = "30px Arial";
+                    ctx.fillStyle = "red";
+                    ctx.fillText(`Dumbbell Curl: ${dumbbellCurlEvaluation}`, 10, 50);
+
+                    const currentTime = Date.now();
+                    if (dumbbellCurlEvaluation === "Perfect" && currentTime - lastCountTime > 2000) {
                         count += 1;
                         lastCountTime = currentTime;
                     }
@@ -211,10 +288,8 @@ async function detectPose(video, net) {
 
             countDisplay.innerText = `Count: ${count}`;
         }
-
         requestAnimationFrame(poseDetectionFrame);
     }
-
     poseDetectionFrame();
 }
 
@@ -232,7 +307,6 @@ function drawKeypoints(keypoints, minConfidence, ctx) {
 
 function drawSkeleton(keypoints, minConfidence, ctx) {
     const adjacentKeyPoints = posenet.getAdjacentKeyPoints(keypoints, minConfidence);
-
     adjacentKeyPoints.forEach((keypoints) => {
         const [{ y: y1, x: x1 }, { y: y2, x: x2 }] = keypoints;
         ctx.beginPath();
@@ -247,7 +321,6 @@ function drawSkeleton(keypoints, minConfidence, ctx) {
 async function main() {
     const video = await setupCamera();
     video.play();
-
     const net = await setupPoseNet(video);
     detectPose(video, net);
 }
